1f738c2a46ad2eaabb33d06e762b38343e963626
Yash Banarase <yashbanarase1455@gmail.com>
2024-11-07 14:35:50 +0530

    final addition

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,7 +1,7 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "pg_life");
+$conn = mysqli_connect("127.0.0.1", "root", "Sachin01!", "pglife");
 
-if (mysqli_connect_error()) {
+if (mysqli_connect_errno()) {
     // Throw error message based on ajax or not
     echo "Failed to connect to MySQL! Please contact the admin.";
     return;
